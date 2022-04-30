44c8c9849e90df49f0aa282ead66b2e843ecd77d
joonas.oispuu <oispuu.joonas@gmail.com>
2022-04-30 19:20:04 +0300

    third commit

---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+	public class ReviewsController : Controller
+	{
+		public ActionResult Index()
+		{
+			var model = from review in _reviews
+						orderby review.Country
+						select review;
+			return View(model);
+		}
+
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		private static List<RestaurantReview> _reviews = new List<RestaurantReview>
+		{
+			new RestaurantReview
+			{
+				Id = 1,
+				Name = "Cinnamon Club",
+				City ="London",
+				Country ="UK",
+				Rating = 10
+			},
+			new RestaurantReview
+			{
+				Id = 2,
+				Name = "Mens club",
+				City ="Berlin",
+				Country ="Germany",
+				Rating = 10
+			},
+			new RestaurantReview
+			{
+				Id = 3,
+				Name = "Cool Food Place",
+				City ="Rome",
+				Country ="Italy",
+				Rating = 10
+			}
+		};
+	}
+}
