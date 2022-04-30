6598ae9d144b44c38be5c0f9c63ca142049b78b0
joonas.oispuu <oispuu.joonas@gmail.com>
2022-04-30 19:09:14 +0300

    first commit

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			var controller = RouteData.Values["controller"];
+			var action = RouteData.Values["action"];
+			var id = RouteData.Values["id"];
+
+			ViewBag.Message = $"{controller}::{action} {id}";
+
+			return View();
+		}
+		public IActionResult About()
+		{
+			var model = new AboutModel()
+			{
+				Name = "Joonas Õispuu",
+				Location = "Tallinn"
+			};
+			return View(model);
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
