70f0b226c1631871831b3a5baad6a99c624ab90d
joonas.oispuu <oispuu.joonas@gmail.com>
2022-05-01 16:39:55 +0300

    eleventh commit

--- OdeToFood/wwwroot/js/site.js
+++ OdeToFood/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+	let ajaxFormSubmit = function () {
+		const $form = $(this);
+		const options = {
+			url: $form.attr("action"),
+			type: $form.attr("method"),
+			data: $form.serialize()
+		}
+		$.ajax(options).done(function (data) {
+			const $target = $($form.attr("data-otf-target"));
+			const $newHtml = $(data);
+			$target.replaceWith($newHtml);
+			$newHtml.effect("highlight");
+		});
+		return false;
+	};
+	const submitAutocompleteForm = function (event, ui) {
+		const $input = $(this);
+		$input.val(ui.item.label);
+		const $form = $input.parents("form:first");
+		$form.submit();
+	};
+	const createAutocomplete = function () {
+		const $input = $(this);
+		const options = {
+			source: $input.attr("data-otf-autocomplete"),
+			select: submitAutocompleteForm
+		};
+		$input.autocomplete(options);
+	};
 
-// Write your JavaScript code.
+	$("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+	$("input[data-otf-autocomplete]").each(createAutocomplete);
+});
